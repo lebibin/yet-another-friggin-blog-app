0925ec8381beb8452bc7b1143c3c06fa7fea3daf
Kevin Candelaria <kevdashdev@gmail.com>
2017-02-12 10:51:02 +0800

    Add timestamps

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post
   extend ActiveModel::Naming
   include ActiveModel::Conversion
-  attr_accessor :title, :body, :blog
+  attr_accessor :title, :body, :blog, :pubdate
   def initialize(attrs={})
     attrs.each do |k, v|
       send("#{k}=", v)
@@ -12,7 +12,8 @@ class Post
     false
   end
 
-  def publish
+  def publish(clock=DateTime)
+    self.pubdate = clock.now
     blog.add_entry(self)
   end
 end
